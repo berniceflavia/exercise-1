b581ccb69867cc5b4525eb4f7819e07f81307872
bernice flavia <berniceflavia@dani-HP-Pavilion-14-Notebook-PC.localdomain>
2018-07-23 21:12:39 +0700

    Exercise-Day-13

---
+++ exercises1.js
@@ -0,0 +1,13 @@
+function square (number) {
+    var square = number * number
+    return square
+  }
+  
+  function pi () {
+    var pi = 3.14
+    return pi
+  }
+  
+  
+  var area = pi() * square(7);
+  console.log(area); // 154
