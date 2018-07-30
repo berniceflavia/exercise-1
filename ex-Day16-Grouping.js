61557ccc89733c4df2873a889d63795f9f8b6314
bernice flavia <berniceflavia@dani-HP-Pavilion-14-Notebook-PC.localdomain>
2018-07-30 20:39:25 +0700

    exericse1-Day16-ObjectLiteral

---
+++ ex-Day16-Grouping.js
@@ -0,0 +1,20 @@
+function grouping (scores) {
+    var grouping = {
+        pass : [],
+        fail : []
+    }
+    for (var i = 0; i< scores.length; i++){
+        if (scores[i] >= 80) {
+            grouping.pass.push(scores[i])
+        } else {
+            grouping.fail.push(scores[i])
+        }
+    }
+    return grouping
+}
+console.log(grouping([80, 45, 90, 65, 74, 100, 85, 30]));
+
+// // { pass: [ 80, 90, 100, 85 ], fail: [ 45, 65, 74, 30 ] }
+
+// console.log(grouping([]));
+// // { pass: [], fail: [] }
