45efe0529eb0164e78d49f7f1324d5d71cab20ba
bernice flavia <berniceflavia@dani-HP-Pavilion-14-Notebook-PC.localdomain>
2018-08-01 20:57:21 +0700

    Berlatih-Object-Dengan-Object-Literal.1

---
+++ ex-Day17-CSVToObjectLiteral.js
@@ -0,0 +1,23 @@
+function csvToObject (text) {
+  var newText = text.split(',')
+  var obj = {
+      name: newText[0],
+      email: newText[1],
+      gender: newText[2]
+  }
+  
+  return obj
+}
+console.log(csvToObject('Icha,icha@mail.com,female'));
+
+// // TEST CASES
+// console.log(csvToObject('Dimitri,dimitri@mail.com,male'));
+// // { name: 'Dimitri', email: 'dimitri@mail.com', gender: 'male' }
+
+// console.log(csvToObject('Icha,icha@mail.com,female'));
+// // { name: 'Icha', email: 'icha@mail.com', gender: 'female' }
+
+// console.log(csvToObject('Dhani,dhani@mail.com,male'));
+// // { name: 'Dhani', email: 'dhani@mail.com', gender: 'male' }
+// Diberikan sebuah function yang menerima 1 parameter berupa string yang berisi value dan dipisahkan dengan koma (Comma Separated Values).
+// Implementasikan function ini supaya bisa mengembalikan value object literal. Sesuaikan dengan format di atas.
